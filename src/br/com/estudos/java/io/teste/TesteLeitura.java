c83b5a8a84554afc0e0b3e7d970ae66cbaf4e955
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-01 21:11:56 -0300

    Criando a classe de teste para leitura de arquivos

--- src/br/com/estudos/java/io/teste/TesteLeitura.java
+++ src/br/com/estudos/java/io/teste/TesteLeitura.java
@@ -1,7 +1,13 @@
 package br.com.estudos.java.io.teste;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class TesteLeitura {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		//input flow with some file
+		FileInputStream fis = new FileInputStream("lorem.txt");
+		
 		
 	}
 

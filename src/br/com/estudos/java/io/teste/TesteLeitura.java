86cc56b3b18fd897036eee69c118d696f09feb91
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-01 21:37:30 -0300

    melhorando o código

--- src/br/com/estudos/java/io/teste/TesteLeitura.java
+++ src/br/com/estudos/java/io/teste/TesteLeitura.java
@@ -5,18 +5,19 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class TesteLeitura {
 	public static void main(String[] args) throws IOException {
 		//input flow with some file
 		FileInputStream fis = new FileInputStream("lorem.txt");
-		InputStreamReader isr = new InputStreamReader(fis);
+		Reader isr = new InputStreamReader(fis);
 		BufferedReader br = new BufferedReader(isr);
-		String linha;
+		String linha = br.readLine();
 		
-		while(br.readLine() != null) {
-			linha = br.readLine();
+		while(linha != null) {
 			System.out.println(linha);
+			linha = br.readLine();
 		}
 		br.close();
 	}

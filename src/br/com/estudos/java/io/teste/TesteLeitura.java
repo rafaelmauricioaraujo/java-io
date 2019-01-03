c6b508f8f736496ad89eccf05cf68b4643eef14b
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-02 21:57:31 -0300

    Correções no nomes das classes

--- src/br/com/estudos/java/io/teste/TesteLeitura.java
+++ src/br/com/estudos/java/io/teste/TesteLeitura.java
@@ -1,24 +1,24 @@
 package br.com.estudos.java.io.teste;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class TesteLeitura {
 	public static void main(String[] args) throws IOException {
 		//input flow with some file
-		OutputStream fos = new FileOutputStream("lorem2.txt");
-		Writer osw = new OutputStreamWriter(fos);
-		BufferedWriter bw = new BufferedWriter(osw);
+		FileInputStream fis = new FileInputStream("lorem.txt");
+		Reader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		String linha = br.readLine();
 		
-		bw.write("Testando a escrita de uma nova linha no arquivo");
-		bw.newLine();
-		bw.newLine();
-		bw.write("lalalalaaalala");
-		
-		bw.close();
+		while(linha != null) {
+			System.out.println(linha);
+			linha = br.readLine();
+		}
+		br.close();
 	}
 }

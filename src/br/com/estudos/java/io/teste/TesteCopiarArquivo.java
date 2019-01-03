08cb1bf08c7834d8be99a6208748c53b3ad56dd7
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-02 22:21:35 -0300

    testes usando o System.in e o System.out

--- src/br/com/estudos/java/io/teste/TesteCopiarArquivo.java
+++ src/br/com/estudos/java/io/teste/TesteCopiarArquivo.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -14,20 +15,21 @@ import java.io.Writer;
 
 public class TesteCopiarArquivo {
 	public static void main(String[] args) throws IOException {
-		//input flow with some file
-		FileInputStream fis = new FileInputStream("lorem.txt");
+		
+		InputStream fis = System.in; //new FileInputStream("lorem.txt");
 		Reader isr = new InputStreamReader(fis);
 		BufferedReader br = new BufferedReader(isr);
 		
-		OutputStream fos = new FileOutputStream("lorem2.txt");
+		OutputStream fos = System.out;//new FileOutputStream("lorem2.txt");
 		Writer osw = new OutputStreamWriter(fos);
 		BufferedWriter bw = new BufferedWriter(osw);
 		
 		String linha = br.readLine();
 		
-		while(linha != null) {
+		while(linha != null && !linha.isEmpty()) {
 			bw.write(linha);
 			bw.newLine();
+			bw.flush();
 			linha = br.readLine();
 		}
 		br.close();

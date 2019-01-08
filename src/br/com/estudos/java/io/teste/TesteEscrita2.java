a4497299262463144e1aa44a879e10f9e60dbf14
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-08 20:57:34 -0300

    testando escrita com uma classe de nível mais alto

---
+++ src/br/com/estudos/java/io/teste/TesteEscrita2.java
@@ -0,0 +1,36 @@
+package br.com.estudos.java.io.teste;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class TesteEscrita2 {
+	public static void main(String[] args) throws IOException {
+
+		
+		//output flow with some file
+		//OutputStream fos = new FileOutputStream("lorem2.txt");
+		//Writer osw = new OutputStreamWriter(fos);
+		//BufferedWriter bw = new BufferedWriter(osw);
+		
+		//bw.write("Testando a escrita de uma nova linha no arquivo");
+		//bw.newLine();
+		//bw.newLine();
+		//bw.write("lalalalaaalala");
+		
+		//bw.close();
+
+		
+		FileWriter fw = new FileWriter("lorem2.txt");
+		fw.write("Testando a escrita com uma classe de mais alto nível");
+		fw.write("\n");
+		fw.write("Lalalalllallalla");
+		
+		
+		
+	}
+}

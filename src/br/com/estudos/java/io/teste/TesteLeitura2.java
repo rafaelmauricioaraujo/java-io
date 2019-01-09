ea74c391ebfc31cc583b2e1263f4d5aaeb16cf2e
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-09 19:25:50 -0300

    formatando strings

--- src/br/com/estudos/java/io/teste/TesteLeitura2.java
+++ src/br/com/estudos/java/io/teste/TesteLeitura2.java
@@ -18,13 +18,13 @@ public class TesteLeitura2 {
 			linhaScanner.useLocale(Locale.US);
 			linhaScanner.useDelimiter(",");
 			
-			String valor1 = linhaScanner.next();
-			int valor2 = linhaScanner.nextInt();
-			int valor3 = linhaScanner.nextInt();
-			String valor4 = linhaScanner.next();
-			double valor5 = linhaScanner.nextDouble();
+			String tipo = linhaScanner.next();
+			int agencia = linhaScanner.nextInt();
+			int conta = linhaScanner.nextInt();
+			String titular = linhaScanner.next();
+			double saldo = linhaScanner.nextDouble();
 			
-			String valorFormatado = String.format("%s %s %s %s %s", valor1, valor2, valor3, valor4, valor5);
+			String valorFormatado = String.format(new Locale("pt", "BR"), "%s %04d %08d %s %010.2f", tipo, agencia, conta, titular, saldo);
 			
 			System.out.println(valorFormatado);
 			

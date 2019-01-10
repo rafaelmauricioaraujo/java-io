cd04d705b826271f8b70b48f3e57d92b9b66731b
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-10 18:20:36 -0300

    Testando padrão Unicode

---
+++ src/br/com/estudos/java/io/teste/TesteUnicodeEEnconding.java
@@ -0,0 +1,12 @@
+package br.com.estudos.java.io.teste;
+
+public class TesteUnicodeEEnconding {
+	
+	public static void main(String[] args) {
+		
+		String s = "CCCC";
+		
+		System.out.println(s.codePointAt(0));
+	}
+
+}

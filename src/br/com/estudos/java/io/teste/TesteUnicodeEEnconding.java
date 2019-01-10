f407cec7844fb29a46a83d942d198089a81c64ae
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-10 19:08:16 -0300

    forçando um problema de econdings

--- src/br/com/estudos/java/io/teste/TesteUnicodeEEnconding.java
+++ src/br/com/estudos/java/io/teste/TesteUnicodeEEnconding.java
@@ -8,7 +8,7 @@ public class TesteUnicodeEEnconding {
 	
 	public static void main(String[] args) throws UnsupportedEncodingException {
 		
-		String s = "C";
+		String s = "ç";
 		
 		//Imprimindo o código Unicode do string s
 		System.out.println(s.codePointAt(0));
@@ -18,24 +18,26 @@ public class TesteUnicodeEEnconding {
 		System.out.println(charset.displayName());
 		
 		byte[] bytes = s.getBytes();
-		System.out.println(bytes.length);
-		
-		bytes = s.getBytes("UTF-8");
-		System.out.println(bytes.length);
-		
-		bytes = s.getBytes("windows-1252");
-		System.out.println(bytes.length);
-		
-		bytes = s.getBytes("UTF-16");
-		System.out.println(bytes.length);
-		
-		//Outra maneira de fazer o mesmo
-		bytes = s.getBytes(StandardCharsets.US_ASCII);
-		System.out.println(bytes.length);
-		
-		//Fazendo o caminho inverso
+		System.out.println(bytes.length + " Padrão");
 		String snovo = new String(bytes);
 		System.out.println(snovo);
 		
+		bytes = s.getBytes("UTF-8");
+		System.out.println(bytes.length + " UTF-8");
+		
+		bytes = s.getBytes("windows-1252");
+		System.out.println(bytes.length + " Windows-1252");
+		snovo = new String(bytes, "UTF-8");
+		System.out.println(snovo);
+		
+		bytes = s.getBytes("UTF-16");
+		System.out.println(bytes.length + " UTF-16");
+		snovo = new String(bytes, "US-ASCII");
+		System.out.println(snovo);
+		
+		//Outra maneira de fazer o mesmo
+		bytes = s.getBytes(StandardCharsets.US_ASCII);
+		System.out.println(bytes.length + " US_ASCII");
+			
 	}
 }

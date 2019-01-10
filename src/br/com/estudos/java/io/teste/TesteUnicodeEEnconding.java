d7e01c64111adff5028d7f528968079816b98b9d
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-10 18:32:48 -0300

    Imprimindo o econding do java para o windows

--- src/br/com/estudos/java/io/teste/TesteUnicodeEEnconding.java
+++ src/br/com/estudos/java/io/teste/TesteUnicodeEEnconding.java
@@ -1,12 +1,19 @@
 package br.com.estudos.java.io.teste;
 
+import java.nio.charset.Charset;
+
 public class TesteUnicodeEEnconding {
 	
 	public static void main(String[] args) {
 		
 		String s = "CCCC";
 		
+		//Imprimindo o código Unicode do string s
 		System.out.println(s.codePointAt(0));
+		
+		//Imprimindo o Ecoding do java para o windows
+		Charset charset = Charset.defaultCharset();
+		System.out.println(charset.displayName());
 	}
 
 }

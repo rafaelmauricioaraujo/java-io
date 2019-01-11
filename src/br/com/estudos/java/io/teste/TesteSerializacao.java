60513141f9677528ebdc688025b08d217aacb234
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-11 16:08:16 -0300

    criando uma classe para conversão de uma string em binario

--- src/br/com/estudos/java/io/teste/TesteSerializacao.java
+++ src/br/com/estudos/java/io/teste/TesteSerializacao.java
@@ -1,11 +1,19 @@
 package br.com.estudos.java.io.teste;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class TesteSerializacao {
 	
-	public static void main(String[] args) {
-		
-		
+	public static void main(String[] args) throws IOException {
+				
 		String nome = "Rafael Araujo";
+		
+		ObjectOutputStream oss = new ObjectOutputStream(new FileOutputStream("objeto.bin"));
+		oss.writeObject(nome);
+		oss.close();
+		
 	}
 
 }

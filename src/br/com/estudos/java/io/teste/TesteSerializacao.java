ac44b0a68d3cb348a59ee72043bae23b183bcb49
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-11 16:11:38 -0300

    adicionando comentários para melhor entendimento

--- src/br/com/estudos/java/io/teste/TesteSerializacao.java
+++ src/br/com/estudos/java/io/teste/TesteSerializacao.java
@@ -8,10 +8,16 @@ public class TesteSerializacao {
 	
 	public static void main(String[] args) throws IOException {
 				
+		//Criando um objeto
 		String nome = "Rafael Araujo";
 		
+		//Instanciando uma classe que transforma o objeto em um arquivo binário
 		ObjectOutputStream oss = new ObjectOutputStream(new FileOutputStream("objeto.bin"));
+		
+		//gravando o objeto criado no arquivo binário
 		oss.writeObject(nome);
+		
+		//fechando o arquivo
 		oss.close();
 		
 	}
